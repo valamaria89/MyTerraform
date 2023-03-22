9ca96b13101b04f6c6d790b71739d74bf66bb29e
Vala <vala.valsdottir@inmeta.no>
2023-03-22 15:54:12 +0100

    terraform repo

---
+++ modules/azm-workspace.tf
@@ -0,0 +1,39 @@
+resource "azurerm_machine_learning_workspace" "mlw" {
+  name                    = "mlw-${var.prefix}-${var.postfix}${var.env}"
+  location                = var.location
+  resource_group_name     = var.rg_name
+  application_insights_id = var.application_insights_id
+  key_vault_id            = var.key_vault_id
+  storage_account_id      = var.storage_account_id
+  container_registry_id   = var.container_registry_id
+
+  sku_name = "Basic"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+}
+
+# Compute cluster
+
+resource "azurerm_machine_learning_compute_cluster" "adl_aml_ws_compute_cluster" {
+  name                          = "cpu-cluster"
+  location                      = var.location
+  vm_priority                   = "LowPriority"
+  vm_size                       = "Standard_DS3_v2"
+  machine_learning_workspace_id = azurerm_machine_learning_workspace.mlw.id
+  count                         = var.enable_aml_computecluster ? 1 : 0
+
+  scale_settings {
+    min_node_count                       = 0
+    max_node_count                       = 4
+    scale_down_nodes_after_idle_duration = "PT120S" # 120 seconds
+  }
+}
+
+
+output "name" {
+  value = azurerm_machine_learning_workspace.mlw.name
+}
---
+++ modules/resourcegroups/resource-groups.tf
@@ -0,0 +1,13 @@
+Vresource "azurerm_resource_group" "adl_rg" {
+  name     = "rg-${var.prefix}-${var.postfix}${var.env}"
+  location = var.location
+  tags     = var.tags
+}
+
+output "name" {
+  value = azurerm_resource_group.adl_rg.name
+}
+
+output "location" {
+  value = azurerm_resource_group.adl_rg.location
+}
