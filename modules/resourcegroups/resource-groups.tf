2cd859476f92313ae93c39aff6a6cd957a05f5e3
Vala <vala.valsdottir@inmeta.no>
2023-03-23 13:20:08 +0100

    made changes

--- modules/resourcegroups/resource-groups.tf
+++ modules/resourcegroups/resource-groups.tf
@@ -1,4 +1,4 @@
-Vresource "azurerm_resource_group" "adl_rg" {
+resource "azurerm_resource_group" "adl_rg" {
   name     = "rg-${var.prefix}-${var.postfix}${var.env}"
   location = var.location
   tags     = var.tags
